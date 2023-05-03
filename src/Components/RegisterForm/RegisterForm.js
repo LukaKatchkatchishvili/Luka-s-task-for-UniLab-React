f0cd2c90d755a098bc5175ac8eee477a082fac90
Luka Katchkatchishvili <luka.katchkatchishvili.1@gmail.com>
2023-05-03 22:09:43 +0400

    main

--- src/Components/RegisterForm/RegisterForm.js
+++ src/Components/RegisterForm/RegisterForm.js
@@ -40,15 +40,15 @@ const RegisterForm = () => {
   };
   return (
     <>
-      <div className="bg-white w-5/6 lg:w-1/3 mx-auto text-center h-[70vh] my-[15vh] p-12 rounded-md">
-        <h1 className="text-5xl">Get Started</h1>
+      <div className="bg-white w-5/6 lg:w-1/3 mx-auto text-center sm:h-[70vh] my-[15vh] p-4 py-10 md:p-12 rounded-md">
+        <h1 className="text-2xl md:text-5xl">Get Started</h1>
         <div className="py-4 flex flex-col justify-around h-full">
           <div>
             <div>
-              <p className="text-2xl">add a photo</p>
+              <p className="text-md md:text-2xl">add a photo</p>
               <div className="uploadimage w-[75px] mx-auto ">
                 <label htmlFor="imgs" className="block w-[75px]">
-                  <BsUpload size={75} className="my-5" />
+                  <BsUpload size={72} className="my-5" />
                 </label>
               </div>
               {notFilledImg ? (
@@ -80,8 +80,8 @@ const RegisterForm = () => {
               }}
             />
           </div>
-          <div>
-            <div className="mx-auto my-3 text-2xl">fill in your name</div>
+          <div className="my-7 md:my-0">
+            <div className="mx-auto text-md md:text-2xl">fill in your name</div>
             <input
               type="text"
               name="name"
@@ -100,7 +100,7 @@ const RegisterForm = () => {
           </div>
           <div>
             <button
-              className="bg-green-300 py-3 px-16  text-2xl rounded-md"
+              className="bg-green-300 py-2 px-12 md:py-3 md:px-16 text-md md:text-2xl rounded-md"
               onClick={handleSignIn}
             >
               Sign In
