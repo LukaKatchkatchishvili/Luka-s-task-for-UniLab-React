e505fbe026ae79d608dcabb7aca079523aa62aca
Luka Katchkatchishvili <luka.katchkatchishvili.1@gmail.com>
2023-05-04 01:06:08 +0400

    main

--- src/Pages/Logined.js
+++ src/Pages/Logined.js
@@ -4,7 +4,6 @@ import TodoApp from "../Components/Todo/TodoApp";
 import { useNavigate } from "react-router-dom";
 const Login = () => {
   const loggedUser = JSON.parse(localStorage.getItem("user"));
-  console.log(loggedUser);
   const navigate = useNavigate();
   useEffect(() => {
     if (loggedUser == null) {
